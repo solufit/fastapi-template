a51adbdb3af4c78d1423348476377c274a4d9506
Yuta Takahashi <y.highbridge04@gmail.com>
2024-07-10 09:26:31 +0000

    chore: Update devcontainer.json, lint-python.yml, settings.json, and pyproject.toml

--- src/app.py
+++ src/app.py
@@ -1,15 +1,8 @@
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-app = FastAPI(
-    title="Templete API",
-    description="Solufit Microservice Network",
-    version="1.0.0"
-)
+
+app = FastAPI(title="Templete API", description="Solufit Microservice Network", version="1.0.0")
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
+    CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
 )
