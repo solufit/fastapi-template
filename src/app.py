e2a144ddb81d9b95a13769ab3fcf0905ff126587
sasanqu_a <sazanka0316@gmail.com>
2024-08-06 18:42:49 +0900

    chore: Update start script to enable debug mode and configure logging

--- src/app.py
+++ src/app.py
@@ -1,3 +1,6 @@
+import os
+
+import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
@@ -5,3 +8,9 @@ app = FastAPI(title="Templete API", description="Solufit Microservice Network", 
 app.add_middleware(
     CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
 )
+
+if __name__ == "__main__":
+    if os.getenv("DEBUG") == "True":
+        uvicorn.run("app:app", host="0.0.0.0", port=80, reload=True, log_config="log_config_debug.yaml")
+    else:
+        uvicorn.run("app:app", host="0.0.0.0", port=80, reload=True, log_config="log_config.yaml")
