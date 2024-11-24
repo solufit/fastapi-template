69927665704fe0564af277a70ae3360f57c8d973
Yuta Takahashi <y.highbridge04@gmail.com>
2024-11-24 09:10:12 +0000

    Add docstrings to UserCreate and UserResponse models

--- src/scheme/user.py
+++ src/scheme/user.py
@@ -2,12 +2,20 @@ from pydantic import BaseModel
 
 
 class UserCreate(BaseModel):
+    """
+    Schema for creating a new user.
+    """
+
     name: str
     fullname: str
     nickname: str
 
 
 class UserResponse(BaseModel):
+    """
+    Represents the response model for a user.
+    """
+
     id: int
     name: str
     fullname: str
